becceda47ade54d33788dac75f4c964351689b38
LassisterV16 <lassisterv16@gmail.com>
2025-04-08 20:38:20 +0200

    added conditionals

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 def copy_file(command: str) -> None:
-    if command[:2] == "cp":
-        command = command.split()
-        try:
-            with (open(command[1], "r") as file_in,
-                  open(command[-1], "x") as file_out):
-                file_out.write(file_in.read())
-        except FileExistsError:
-            pass
-        except FileNotFoundError:
-            pass
+    command = command.split()
+    if len(command) != 3:
+        return None
+    if command[0] != "cp":
+        return None
+    try:
+        with (open(command[1], "r") as file_in,
+              open(command[-1], "x") as file_out):
+            file_out.write(file_in.read())
+    except FileExistsError:
+        pass
+    except FileNotFoundError:
+        pass
