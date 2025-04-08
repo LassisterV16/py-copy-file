7aefa2a1fe23b3e6dc6bc816bfdd81ac5009e6ed
LassisterV16 <lassisterv16@gmail.com>
2025-04-08 20:33:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if command[:2] == "cp":
+        command = command.split()
+        try:
+            with (open(command[1], "r") as file_in,
+                  open(command[-1], "x") as file_out):
+                file_out.write(file_in.read())
+        except FileExistsError:
+            pass
+        except FileNotFoundError:
+            pass
